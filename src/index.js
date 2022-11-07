db42b54a210afe15b49107e1588d17283ffff4bd
Victor Blanco <12vblanco@gmail.com>
2022-11-07 15:48:54 +0000

    router issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
