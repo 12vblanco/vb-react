4b18425fa3b02d0a1615927e24b045d49faebfc2
Victor Blanco <12vblanco@gmail.com>
2022-11-10 01:06:38 +0000

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,10 +7,26 @@ const Form = (props) => {
     <MainContainer>
       <H1>Please send us any requests or queries using this form:</H1>
       <FromContainer>
-        <form name="contact" netlify netlify-honeypot="bot-field">
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <textarea name="message"></textarea>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
         {/* <form
           name="BasketForm"
