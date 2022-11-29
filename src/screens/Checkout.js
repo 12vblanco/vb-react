b65a26bed3a38f2ec330981715741e07ab17766e
Victor Blanco <12vblanco@gmail.com>
2022-11-29 16:40:13 +0000

    stripe

--- src/screens/Checkout.js
+++ src/screens/Checkout.js
@@ -6,7 +6,8 @@ const stripe = Stripe(
   "pk_test_51HqgwdGKpDMhyEuL11A63hDc42CNdjZbMH93xDPIumVyYlgGe5byVF9rXhgW0rs64r0uaDjQUqlwOUDXrbTZy9nx00cyCIwiBm"
 );
 
-const callApi = () => {
+const callApi = (e) => {
+  e.preventDefault();
   fetch("/api/stripe", {
     method: "POST",
   })
