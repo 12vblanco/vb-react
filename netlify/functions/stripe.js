7fb9cb2661cef11c61301abc3a190f6ff66c5f16
Victor Blanco <12vblanco@gmail.com>
2022-12-07 03:38:06 +0000

    netlify

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -1,6 +1,6 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 
-exports.handler = async () => {
+exports.handler = async (cart) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -23,6 +23,7 @@ exports.handler = async () => {
     statusCode: 200,
     body: JSON.stringify({
       id: session.id,
+      items: cart.items,
     }),
   };
 };
