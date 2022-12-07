1cfcd521cf740387772f06f8d24a40e31926feea
Victor Blanco <12vblanco@gmail.com>
2022-12-07 03:43:17 +0000

    netlify

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -23,7 +23,7 @@ exports.handler = async (cart) => {
     statusCode: 200,
     body: JSON.stringify({
       id: session.id,
-      items: "cart.items",
+      items: cart.items,
     }),
   };
 };
